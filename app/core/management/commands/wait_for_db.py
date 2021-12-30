9644ec967b3753a7818aa29f9fc3522e9bff8788
Anandh Ravindran <anandh.ravindran@gmail.com>
2021-12-30 04:27:48 -0800

    Checking for psycopg2 operational error in wait_for_db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,7 @@
 import time
 
+from psycopg2 import OperationalError as Psycopg2OpError
+
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
@@ -14,7 +16,7 @@ class Command(BaseCommand):
         while not db_conn:
             try:
                 db_conn = connections['default']
-            except OperationalError:
+            except (OperationalError, Psycopg2OpError):
                 self.stdout.write('Database unavailable, waiting for 1 sec')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available'))
